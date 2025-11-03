58ef6cff9d40c2417e871ecbedc3ae8a6a19ede5
slea7y <maak.kijewskaa@op.pl>
2025-11-03 15:19:07 +0100

    FIX(some idea of the struct for the scene)

--- Includes/minirt.h
+++ Includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 14:55:21 by majkijew          #+#    #+#             */
-/*   Updated: 2025/11/02 15:09:39 by majkijew         ###   ########.fr       */
+/*   Updated: 2025/11/03 15:18:26 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,41 @@ int		ft_isspace(char c);
 double	atod(char *s);
 
 
-// stuct 
+//scene struct
+typedef struct s_amb_light
+{
+	double	ratio;
+	int		colors[3];
+}			t_amb_light;
+
+typedef struct s_vec
+{
+	double	x;
+	double	z;
+	double	y;
+}			t_vec;
+
+typedef struct s_camera
+{
+	t_vec	orintation_vector;
+	t_vec	position;
+	double	horizontal_field;
+}				t_camera;
+
+typedef	struct s_light
+{
+	t_vec	cord_light; //coordinates of the lightning point
+	double	bright_ratio;
+	int		colors[3];
+}				t_light;
+
+typedef	struct s_scene_data
+{
+	// mlx_t		mlx;
+	// mlx_image_t		*img; those two for the mlx window
+	t_amb_light	amb_light;
+	t_camera	camera;
+	t_light		light;
+}				t_scene_data;
 
 #endif
