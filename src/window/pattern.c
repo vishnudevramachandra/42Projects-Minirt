b257fb46b4b7996f865265e9e51189a5f5223bab
Vishnudev Ramachandra <vramacha@2-C-1.42heilbronn.de>
2026-01-09 14:31:42 +0100

    fix[pattern]: added the pattern.c file that was missing in the previous commit

---
+++ src/window/pattern.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pattern.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vramacha <vramacha@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 11:16:55 by vramacha          #+#    #+#             */
+/*   Updated: 2026/01/09 10:43:31 by vramacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../Includes/minirt.h"
+
+void	hadamard_prod(t_tup res, t_tup a, t_tup b)
+{
+	int		i;
+
+	i = 0;
+	while (i < 4)
+	{
+		res[i] = a[i] * b[i];
+		i++;
+	}
+}
+
+t_rgb	*stripped_pattern(t_tup param, t_rgb *c1, t_rgb *c2, t_tup hit_point)
+{
+	if ((int)floor(dot_prod(param, hit_point)) % 2)
+		return (c2);
+	else
+		return (c1);
+}
+
+t_rgb	*ring_pattern(t_tup param, t_rgb *c1, t_rgb *c2, t_tup hit_point)
+{
+	t_tup	res;
+
+	hadamard_prod(res, param, hit_point);
+	if ((int)sqrt(dot_prod(res, res)) % 2)
+		return (c2);
+	else
+		return (c1);
+}
+
+t_rgb	*checker_pattern(t_tup param, t_rgb *c1, t_rgb *c2, t_tup hit_point)
+{
+	t_tup	res;
+
+	hadamard_prod(res, param, hit_point);
+	if ((int)(floor(res[0]) + floor(res[1]) + floor(res[2])) % 2)
+		return (c2);
+	else
+		return (c1);
+}
+
+// int	main()
+// {
+// 	t_tup	param = {1,0,0,0};
+// 	t_rgb	c1 = {255,0,0};
+// 	t_rgb	c2 = {0,255,0};
+// 	t_rgb	*res;
+
+// 	res = stripped_pattern(param, &c1, &c2, (t_tup){-1.3,0,0,1});
+// 	printf("%d,%d,%d\n", res->r, res->g, res->b);
+// 	res = stripped_pattern(param, &c1, &c2, (t_tup){-0.5,0,0,1});
+// 	printf("%d,%d,%d\n", res->r, res->g, res->b);
+// 	res = stripped_pattern(param, &c1, &c2, (t_tup){0.9,0,0,1});
+// 	printf("%d,%d,%d\n", res->r, res->g, res->b);
+// 	res = stripped_pattern(param, &c1, &c2, (t_tup){1.2,0,0,1});
+// 	printf("%d,%d,%d\n", res->r, res->g, res->b);
+// }
