54309955114ce094b5dd18049aea7011a30d0cd1
Maja Kijewska <majkijew@student.42heilbornn.de>
2026-01-31 01:49:06 +0100

    norm[mandatory: ray_inter file]

---
+++ mandatory/ray_inter/create_rays.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_rays.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vramacha <vramacha@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/19 17:12:14 by majkijew          #+#    #+#             */
+/*   Updated: 2026/01/19 14:38:12 by vramacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//chapter 5
+
+#include "minirt.h"
+//						(origin)	(direction)
+void	create_ray(t_ray *ray, t_tup point, t_tup vector)
+{
+	copy_tup(ray->origin, point);
+	copy_tup(ray->direction, vector);
+}
+
+// t_tup	position(t_ray *ray, double t)
+// {
+// 	return ((t_tup){ray->origin[0] + ray->direction[0] * t,
+// 		ray->origin[1] + ray->direction[1] * t,
+// 		ray->origin[2] + ray->direction[2] * t,
+// 		1});
+// }
+
+// void	position(t_tup position, t_ray *ray, double t)
+// {
+// 	init_point(position, 0, 0, 0);
+// 	multi_tuple(position, ray, t);
+// 	add_tuples(position, ray->origin, ray->direction);
+// }
